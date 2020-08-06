d0c70f28441a9eb4e892beff3ae38a19ccf669b9
Martin <martindelacruzboza@gmail.com>
2020-08-06 10:29:33 -0500

    q seg

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 
 
 @Module({
-  imports: [ProductosModule,MongooseModule.forRoot(`mongodb+srv://user:EYKboPnuic8dOLjz@cluster0.n8zpf.mongodb.net/productos?retryWrites=true&w=majority`)],
+  imports: [ProductosModule,MongooseModule.forRoot(`mongodb+srv://@cluster0.n8zpf.mongodb.net/productos?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
